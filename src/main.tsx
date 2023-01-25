5bcd6d598de91e63dac0300f92c22df6f73b2137
xing-6464 <xing_6464@foxmail.com>
2023-01-25 21:22:20 +0800

    初始化代码，并且封装axios

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
 import App from './App'
-import './index.css'
+import './assets/styles/reset.scss'
+import './assets/styles/iconfont.scss'
+import './assets/styles/common.scss'
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
