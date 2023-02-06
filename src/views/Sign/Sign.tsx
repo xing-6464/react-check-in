e77500f3382d7648f306e4b0ae4d9b239cd27f88
xing-6464 <xing_6464@foxmail.com>
2023-02-06 18:55:16 +0800

    描述列表数据渲染与签到联动

--- src/views/Sign/Sign.tsx
+++ src/views/Sign/Sign.tsx
@@ -1,5 +1,14 @@
 import React, { useState, useEffect } from 'react'
-import { Descriptions, Button, Tag, Calendar, Row, Space, Select, message } from 'antd'
+import {
+  Descriptions,
+  Button,
+  Tag,
+  Calendar,
+  Row,
+  Space,
+  Select,
+  message,
+} from 'antd'
 import 'dayjs/locale/zh-cn'
 import locale from 'antd/es/date-picker/locale/zh_CN'
 import type { Dayjs } from 'dayjs'
@@ -8,7 +17,11 @@ import _ from 'lodash'
 
 import styles from './Sign.module.scss'
 import { useAppDispatch, useAppSelector } from '../../store'
-import { getTimeAction, putTimeAction, updateInfos } from '../../store/modules/signs'
+import {
+  getTimeAction,
+  putTimeAction,
+  updateInfos,
+} from '../../store/modules/signs'
 import type { Infos } from '../../store/modules/signs'
 import { toZero } from '../../utils/common'
 
@@ -32,7 +45,7 @@ const originDetailValue: Record<keyof typeof DetailKey, number> = {
   lateAndEarly: 0,
 }
 
-const detailState = {
+const originDetailState = {
   type: 'success' ? 'success' : 'error',
   text: '正常' ? '正常' : '异常',
 }
@@ -43,34 +56,98 @@ export default function Sign() {
   const signsInfos = useAppSelector((s) => s.signs.infos)
   const usersInfos = useAppSelector((s) => s.users.infos)
   const dispatch = useAppDispatch()
+  const [detailValue, setDetailValue] = useState({ ...originDetailValue })
+  const [detailState, setDetailState] = useState({ ...originDetailState })
 
   useEffect(() => {
     if (_.isEmpty(signsInfos)) {
-      dispatch(getTimeAction({ userid: usersInfos._id as string })).then((action) => {
-        const { errcode, infos } = (action.payload as { [index: string]: unknown })
-          .data as {
+      dispatch(getTimeAction({ userid: usersInfos._id as string })).then(
+        (action) => {
+          const { errcode, infos } = (
+            action.payload as { [index: string]: unknown }
+          ).data as {
+            [index: string]: unknown
+          }
+
+          if (errcode === 0) {
+            dispatch(updateInfos(infos as Infos))
+          }
+        }
+      )
+    }
+  }, [signsInfos, usersInfos, dispatch])
+
+  useEffect(() => {
+    if (signsInfos.detail) {
+      const detailMonth = (signsInfos.detail as { [index: string]: unknown })[
+        toZero(month + 1)
+      ] as {
+        [index: string]: unknown
+      }
+      for (let attr in detailMonth) {
+        switch (detailMonth[attr]) {
+          case DetailKey.normal:
+            originDetailValue.normal++
+            break
+          case DetailKey.absent:
+            originDetailValue.absent++
+            break
+          case DetailKey.early:
+            originDetailValue.early++
+            break
+          case DetailKey.late:
+            originDetailValue.late++
+            break
+          case DetailKey.lateAndEarly:
+            originDetailValue.lateAndEarly++
+            break
+          case DetailKey.miss:
+            originDetailValue.miss++
+            break
+        }
+      }
+      setDetailValue({ ...originDetailValue })
+
+      for (const attr in originDetailValue) {
+        if (
+          attr !== 'normal' &&
+          originDetailValue[attr as keyof typeof originDetailValue] !== 0
+        ) {
+          setDetailState({
+            type: 'error',
+            text: '异常',
+          })
+        }
+      }
+    }
+
+    return () => {
+      // 更新前触发或者销毁
+      setDetailState({
+        type: 'success',
+        text: '正常',
+      })
+      for (let attr in originDetailValue) {
+        originDetailValue[attr as keyof typeof originDetailValue] = 0
+      }
+    }
+  }, [month, signsInfos])
+
+  const handlePutTime = () => {
+    dispatch(putTimeAction({ userid: usersInfos._id as string })).then(
+      (action) => {
+        const { errcode, infos } = (
+          action.payload as { [index: string]: unknown }
+        ).data as {
           [index: string]: unknown
         }
 
         if (errcode === 0) {
           dispatch(updateInfos(infos as Infos))
+          message.success('签到成功')
         }
-      })
-    }
-  }, [signsInfos, usersInfos, dispatch])
-
-  const handlePutTime = () => {
-    dispatch(putTimeAction({ userid: usersInfos._id as string })).then((action) => {
-      const { errcode, infos } = (action.payload as { [index: string]: unknown })
-        .data as {
-        [index: string]: unknown
       }
-
-      if (errcode === 0) {
-        dispatch(updateInfos(infos as Infos))
-        message.success('签到成功')
-      }
-    })
+    )
   }
 
   const handleToException = () => {
@@ -80,8 +157,11 @@ export default function Sign() {
   const dateCellRender = (value: Dayjs) => {
     const month =
       signsInfos.time &&
-      (signsInfos.time as { [index: string]: unknown })[toZero(value.month() + 1)]
-    const date = month && (month as { [index: string]: unknown })[toZero(value.date())]
+      (signsInfos.time as { [index: string]: unknown })[
+        toZero(value.month() + 1)
+      ]
+    const date =
+      month && (month as { [index: string]: unknown })[toZero(value.date())]
     let ret = ''
 
     if (Array.isArray(date)) {
@@ -102,7 +182,7 @@ export default function Sign() {
         <Descriptions.Item label='月份'>{month + 1}月</Descriptions.Item>
         {Object.entries(DetailKey).map((v) => (
           <Descriptions.Item key={v[0]} label={v[1]}>
-            {originDetailValue[v[0] as keyof typeof DetailKey]}
+            {detailValue[v[0] as keyof typeof DetailKey]}
           </Descriptions.Item>
         ))}
         <Descriptions.Item label='操作'>
