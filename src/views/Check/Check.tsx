f0505c841a08853d46bad8aa08d4784a7b60b3cc
xing-6464 <xing_6464@foxmail.com>
2023-02-12 09:31:17 +0800

    我的审批页功能实现与更新审批信息

--- src/views/Check/Check.tsx
+++ src/views/Check/Check.tsx
@@ -1,11 +1,17 @@
-import React, { useState } from 'react'
-import { Button, Divider, Input, Radio, Row, Space, Table } from 'antd'
-import { SearchOutlined } from '@ant-design/icons'
+import React, { useEffect, useState } from 'react'
+import { Button, Divider, Input, message, Radio, Row, Space, Table } from 'antd'
+import { CheckOutlined, CloseOutlined, SearchOutlined } from '@ant-design/icons'
+import _ from 'lodash'
 import type { RadioChangeEvent } from 'antd'
 import type { ColumnsType } from 'antd/es/table'
 
 import styles from './Check.module.scss'
 import { useAppSelector, useAppDispatch } from '../../store'
+import {
+  getApplyAction,
+  putApplyAction,
+  updateCheckList,
+} from '../../store/modules/checks'
 import type { Infos } from '../../store/modules/checks'
 
 const approverTypes = [
@@ -16,54 +22,129 @@ const approverTypes = [
 ]
 const defaultType = approverTypes[0].value
 
-const columns: ColumnsType<Infos> = [
-  {
-    title: '申请人',
-    dataIndex: 'applicantname',
-    key: 'applicantname',
-    width: 180,
-  },
-  {
-    title: '审批事由',
-    dataIndex: 'reason',
-    key: 'reason',
-    width: 180,
-  },
-  {
-    title: '时间',
-    dataIndex: 'time',
-    key: 'time',
-    render(_) {
-      return _.join('-')
-    },
-  },
-  {
-    title: '备注',
-    dataIndex: 'note',
-    key: 'note',
-  },
-  {
-    title: '审批人',
-    dataIndex: 'approvername',
-    key: 'approvername',
-    width: 180,
-  },
-  {
-    title: '状态',
-    dataIndex: 'state',
-    key: 'state',
-    width: 180,
-  },
-]
-
 export default function Check() {
   const [approverType, setApproverType] = useState(defaultType)
   const [searchWord, setSearchWord] = useState('')
+
+  const dispatch = useAppDispatch()
+  const usersInfos = useAppSelector((s) => s.users.infos)
   const checkList = useAppSelector((s) => s.checks.checkList).filter(
     (v) =>
       (v.state === approverType || defaultType === approverType) &&
       (v.note as string).includes(searchWord)
   )
+
+  useEffect(() => {
+    if (_.isEmpty(checkList)) {
+      dispatch(getApplyAction({ approverid: usersInfos._id as string })).then(
+        (action) => {
+          const { errcode, rets } = (
+            action.payload as { [index: string]: unknown }
+          ).data as {
+            [index: string]: unknown
+          }
+
+          if (errcode === 0) {
+            dispatch(updateCheckList(rets as Infos[]))
+          }
+        }
+      )
+    }
+  }, [checkList, usersInfos, dispatch])
+
+  const handlePutApply = (_id: string, state: '已通过' | '未通过') => {
+    return () => {
+      dispatch(putApplyAction({ _id, state })).then((action) => {
+        const { errcode } = (action.payload as { [index: string]: unknown })
+          .data as {
+          [index: string]: unknown
+        }
+
+        if (errcode === 0) {
+          message.success('审批成功')
+
+          dispatch(
+            getApplyAction({ approverid: usersInfos._id as string })
+          ).then((action) => {
+            const { errcode, rets } = (
+              action.payload as { [index: string]: unknown }
+            ).data as {
+              [index: string]: unknown
+            }
+
+            if (errcode === 0) {
+              dispatch(updateCheckList(rets as Infos[]))
+            }
+          })
+        }
+      })
+    }
+  }
+
+  const columns: ColumnsType<Infos> = [
+    {
+      title: '申请人',
+      dataIndex: 'applicantname',
+      key: 'applicantname',
+      width: 180,
+    },
+    {
+      title: '审批事由',
+      dataIndex: 'reason',
+      key: 'reason',
+      width: 180,
+    },
+    {
+      title: '时间',
+      dataIndex: 'time',
+      key: 'time',
+      render(_) {
+        return _.join('-')
+      },
+    },
+    {
+      title: '备注',
+      dataIndex: 'note',
+      key: 'note',
+    },
+    {
+      title: '操作',
+      dataIndex: 'handle',
+      key: 'handle',
+      width: 180,
+      render(_, record) {
+        return (
+          <Space>
+            <Button
+              type='primary'
+              shape='circle'
+              size='small'
+              icon={<CheckOutlined />}
+              style={{
+                backgroundColor: '#67c23a',
+                border: '1px #67c23a solid',
+              }}
+              onClick={handlePutApply(record._id as string, '已通过')}
+            />
+            <Button
+              type='primary'
+              danger
+              shape='circle'
+              size='small'
+              icon={<CloseOutlined />}
+              onClick={handlePutApply(record._id as string, '未通过')}
+            />
+          </Space>
+        )
+      },
+    },
+    {
+      title: '状态',
+      dataIndex: 'state',
+      key: 'state',
+      width: 180,
+    },
+  ]
 
   const approverTypeChanger = (ev: RadioChangeEvent) => {
     setApproverType(ev.target.value)
@@ -75,8 +156,7 @@ export default function Check() {
 
   return (
     <div>
-      <Row className={styles['check-title']} justify='space-between'>
-        <Button type='primary'>添加审批</Button>
+      <Row className={styles['check-title']} justify='end'>
         <Space>
           <Input
             placeholder='请输入关键词'
